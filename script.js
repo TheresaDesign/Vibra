fdd2bd32def4d746a280130bb437bf735ca66811
TheresaDesign <resab@t-online.de>
2025-07-18 07:57:43 +0200

    adding popup

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("openPopup").addEventListener("click", function () {
+    document.getElementById("popup").classList.remove("hidden");
+});
+
+document.getElementById("closePopup").addEventListener("click", function () {
+    document.getElementById("popup").classList.add("hidden");
+});
